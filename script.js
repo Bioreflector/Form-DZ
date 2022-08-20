e35c926d2b357ea2e6778d915b6547eb8226eae4
Bioreflector <Bioreflector@gmail.com>
2022-08-20 12:08:18 +0300

    status change fix

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 function FormVerefication(form){
     const formBtn = form.querySelector('.form-btn')
-    let status
+    const inputsForm =form.querySelectorAll('input')
+    const status = []
+
+
+    function checkingForEmptyFields(){
+        inputsForm.forEach((input) =>{
+            if(input.value.trim() === ''){
+                input.parentElement.querySelector('.error-message').innerText = 'The field should not be empty'
+                status.push(false)
+            }
+        })
+    }
     
     function checkUserName(){
         const {userName} = form
         const message = userName.parentElement.querySelector('.error-message')
         if(userName.value.trim() === ""){
         message.innerText = 'Fild Name empty'
-        status = false
+        status.push(false)
         }
         else if(userName.value.length !== 0 && userName.value.length < 3){
         message.innerText = 'Name is short'
-            status = false
+        status.push(false)
         }
         else{
             message.innerText = ''
-            status = true
         }
     }
 
@@ -25,15 +35,14 @@ function FormVerefication(form){
        const re = /\S+@\S+\.\S+/
        if(email.value.trim() === ""){
         message.innerText = 'Fild email empty'
-        status = false
+        status.push(false)
        }
        else if (!re.test(email.value) && email.value.length !== 0 ){
         message.innerText = 'Not correct email address'
-        status = false
+        status.push(false)
        }
        else{
         message.innerText = ''
-        status = true
        }
     }
     function checkPassword(){
@@ -42,25 +51,30 @@ function FormVerefication(form){
         const{confirmPassword} = form
         if(password.value.trim() === ""){
            message.innerText = 'Fild password empty'
-            status = false
+           status.push(false)
         }
         else if(password.value.length < 8){
            message.innerText = 'Password must be at least 8 characters'
-            status = false
+           status.push(false)
         }
         else if(password.value !== confirmPassword.value){
             confirmPassword.parentElement.querySelector('.error-message').innerText = 'Passwords do not match'
-            status = false
+            status.push(false)
         }
         else{
             confirmPassword.parentElement.querySelector('.error-message').innerText = ''
             message.innerText = ''
-            status = true
+           
         }
+    }
+
+    function clearInput(){
+        inputsForm.forEach(input => input.value = '')
     }
 
     function vereficationForm(event){
         event.preventDefault()
+        // checkingForEmptyFields()
         checkUserName()
         checkEmail()
         checkPassword()
@@ -69,13 +83,14 @@ function FormVerefication(form){
 
 
 
-        if(status === false){
-           
+        if(status.includes(false)){
             console.log('ERROR form not validated')
+            status.splice(0)
+            
         }
         else{
             console.log('The form has been validated')
-            
+            clearInput()
         }
 
     }
